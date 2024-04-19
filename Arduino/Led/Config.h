2112cc8f0c7cc860ac906368db323f3061265a38
Deniz <deniz@demirdelen.net>
2024-04-19 18:10:34 +0300

    Update Config.h

--- Arduino/Led/Config.h
+++ Arduino/Led/Config.h
@@ -8,19 +8,19 @@
 
 //Park çizgisi ışıklarının bağlandığı pinler
 //12 adet dijital, 1 adet analog çıkış
-#define LED_1 2
-#define LED_2 3
-#define LED_3 4
-#define LED_4 5
-#define LED_5 6
-#define LED_6 7
-#define LED_7 8
-#define LED_8 9
-#define LED_9 10
-#define LED_10 11
-#define LED_11 12
-#define LED_12 13
-#define LED_13 A5
+#define LED_1 A5
+#define LED_2 A4
+#define LED_3 2
+#define LED_4 3
+#define LED_5 4
+#define LED_6 5
+#define LED_7 6
+#define LED_8 7
+#define LED_9 8
+#define LED_10 9
+#define LED_11 10
+#define LED_12 11
+#define LED_13 12
 
 //Boş park alanı gösterilirken LED'lerin ne kadar sıklıkta ve
 //ne kadar süre yanık kalacağı
